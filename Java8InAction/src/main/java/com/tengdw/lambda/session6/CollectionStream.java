ba07791891fb3c076fe2ff8748bcfeb5c799c34a
TengDW <t_dw@qq.com>
2018-10-23 09:22:45 +0800

    Java 8实战

--- Java8InAction/src/main/java/com/tengdw/lambda/session6/CollectionStream.java
+++ Java8InAction/src/main/java/com/tengdw/lambda/session6/CollectionStream.java
@@ -3,6 +3,7 @@ package com.tengdw.lambda.session6;
 import com.tengdw.pojo.Dish;
 
 import java.util.*;
+import java.util.stream.IntStream;
 
 import static com.tengdw.pojo.Dish.menu;
 import static java.util.Comparator.*;
@@ -58,7 +59,8 @@ public class CollectionStream {
                 .collect(groupingBy(Dish::getType));
         System.out.println(dishesByType);
         // 按照热量分组
-        Map<CaloricLevel, List<Dish>> dishesByCaloricLevel = menu.stream().collect(groupingBy(dish -> groupByCaloricLevel(dish)
+        Map<CaloricLevel, List<Dish>> dishesByCaloricLevel = menu.stream()
+                .collect(groupingBy(dish -> groupByCaloricLevel(dish)
         ));
         System.out.println(dishesByCaloricLevel);
         /*多级分组*/
@@ -71,17 +73,54 @@ public class CollectionStream {
                 .collect(groupingBy(Dish::getType, counting()));
         System.out.println(typeCounting);
         System.out.println(Long.MAX_VALUE);
+        /* 把收集器的结果转换为另一种类型*/
         //查找每个子组中热量最高的Dish
         Map<Dish.Type, Dish> mostCaloricByType = menu.stream()
                 .collect(groupingBy(Dish::getType,
                         collectingAndThen(
-                                maxBy(
-                                        comparingInt(Dish::getCalories)),
+                                maxBy(comparingInt(Dish::getCalories)),
                                 Optional::get)));
         System.out.println(mostCaloricByType);
+        //分组后的求子组的热量总和
+        Map<Dish.Type, Integer> totalCaloriesByType = menu.stream()
+                .collect(groupingBy(Dish::getType,
+                        summingInt(Dish::getCalories)));
+        System.out.println(totalCaloriesByType);
+        //使用groupingBy()和mapping()将子组内的每道菜映射对应的 CaloricLevel
+        Map<Dish.Type, HashSet<CaloricLevel>> caloricLevelsByType = menu.stream()
+                .collect(groupingBy(Dish::getType, mapping(dish -> groupByCaloricLevel(dish),
+                        toCollection(HashSet::new))));
+        System.out.println(caloricLevelsByType);
+        /**分区*/
+        // 把素食和非素食分开
+        Map<Boolean, List<Dish>> partitionedMenu = menu.stream()
+                .collect(partitioningBy(Dish::isVegetarian));
+        System.out.println(partitionedMenu);
+        //素食和非素食中热量最高的菜
+        Map<Boolean, Dish> mostCaloricPartitionedByVegetarian = menu.stream()
+                .collect(partitioningBy(Dish::isVegetarian,
+                        collectingAndThen(maxBy(comparingInt(Dish::getCalories)),
+                                Optional::get)));
+        System.out.println(mostCaloricPartitionedByVegetarian);
+        System.out.println(partitionPrimes(100));
+
     }
 
-    enum CaloricLevel { DIET, NORMAL, FAT }
+    public static Map<Boolean, List<Integer>> partitionPrimes(int n) {
+        return IntStream.rangeClosed(2, n)
+                .boxed()
+                .collect(partitioningBy(candidate -> isPrime(candidate)));
+    }
+
+    public static boolean isPrime(int candidate) {
+        //求出待测数平方根的因子
+        int candidateRoot = (int) Math.sqrt(candidate);
+        // 只要计算从2开始到 candidateRoot 有没有数字可以被 candidate 整除，我们就能知道 candidate 是不是质数
+        return IntStream.rangeClosed(2, candidateRoot)
+                .noneMatch(i -> candidate % i == 0);
+    }
+
+    enum CaloricLevel {DIET, NORMAL, FAT}
 
     public static CaloricLevel groupByCaloricLevel(Dish dish) {
         if (dish.getCalories() <= 400) {
