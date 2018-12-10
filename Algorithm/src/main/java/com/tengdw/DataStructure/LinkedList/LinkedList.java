4915d625e4322c0c6fcb9e29c0b5d1d18cd50a2c
Tengdw <t_dw@qq.com>
2018-12-10 23:20:56 +0800

    数据结构

---
+++ Algorithm/src/main/java/com/tengdw/DataStructure/LinkedList/LinkedList.java
@@ -0,0 +1,166 @@
+package com.tengdw.DataStructure.LinkedList;
+
+/**
+ * 链表
+ *
+ * @author: Tengdw t_dw@qq.com
+ * @create: 2018-12-09 23:47
+ **/
+public class LinkedList<E> {
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    //虚拟头结点
+    private Node dummyHead;
+    private int size;
+
+    public LinkedList() {
+        dummyHead = new Node();
+        size = 0;
+    }
+
+    /**
+     * 获取链表中的元素个数
+     *
+     * @return
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * 返回链表是否为空
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 在链表index位置添加新的元素e （非常用操作）
+     *
+     * @param index
+     * @param e
+     */
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Illegal index.");
+        }
+        // prev 为索引位的前一个元素，存在dummyHead 遍历时不需要 index - 1
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+        prev.next = new Node(e, prev.next);
+        size++;
+    }
+
+    /**
+     * 在链表头添加新的元素e
+     *
+     * @param e
+     */
+    public void addFirst(E e) {
+        add(0, e);
+        size++;
+    }
+
+    /**
+     * 在链表末尾添加新的元素e
+     *
+     * @param e
+     */
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    /**
+     * 获取第index个位置的元素
+     * @param index
+     * @return
+     */
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. Illegal index.");
+        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    public E getLast() {
+        return get(size);
+    }
+
+    /**
+     * 修改链表第index个位置的元素为e
+     * @param index
+     * @param e
+     */
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Set failed. Illegal index.");
+        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    /**
+     * 查找链表中是否有元素e
+     * @param e
+     * @return
+     */
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e.equals(e)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            res.append(cur + "->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
+}
