23e63d93d0a622b4ce81ae42017dca9758a23d81
Tengdw <t_dw@qq.com>
2019-01-02 23:21:46 +0800

    数据结构-哈希表

--- Algorithm/src/main/java/com/tengdw/DataStructure/HashTable/HashTable.java
+++ Algorithm/src/main/java/com/tengdw/DataStructure/HashTable/HashTable.java
@@ -1,28 +1,36 @@
 package com.tengdw.DataStructure.HashTable;
 
+import java.util.Hashtable;
+import java.util.Map;
 import java.util.TreeMap;
 
 /**
  * 哈希表
+ *
  * @author: Tengdw t_dw@qq.com
  * @create: 2019-01-01 19:39
  **/
 public class HashTable<K, V> {
 
-    private TreeMap<K, V>[] hashtable;
-    private int M;
-    private int size;
+    private static final int[] capacity = {53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593, 49157,
+            98317, 196613, 393241, 786433, 1572869, 3145739, 6291469, 12582917, 25165843, 50331653,
+            10663319, 201326611, 402653189, 805306457, 1610612741};
 
-    public HashTable(int M) {
-        this.M = M;
+    //数组最大容量(upperTol * M)和最小容量(lowerTol * M)
+    private static final int upperTol = 10;
+    private static final int lowerTol = 2;
+    private int capacityIndex = 0;
+
+    private TreeMap<K, V>[] hashtable;
+    private int M; //hashtable的长度
+    private int size; //元素数量
+
+    public HashTable() {
+        this.M = capacity[capacityIndex];
         size = 0;
         hashtable = new TreeMap[M];
         for (int i = 0; i < M; i++)
             hashtable[i] = new TreeMap<>();
-    }
-
-    public HashTable() {
-        this(97);
     }
 
     public int getSize() {
@@ -36,6 +44,10 @@ public class HashTable<K, V> {
         } else {
             map.put(key, value);
             size++;
+            if (size >= upperTol * M && capacityIndex + 1 < capacity.length) {
+                capacityIndex++;
+                resize(capacity[capacityIndex]);
+            }
         }
     }
 
@@ -45,6 +57,10 @@ public class HashTable<K, V> {
         if (map.containsKey(key)) {
             ret = map.remove(key);
             size--;
+            if (size < lowerTol * M && capacityIndex - 1 >= 0) {
+                capacityIndex--;
+                resize(capacity[capacityIndex]);
+            }
         }
         return ret;
     }
@@ -66,5 +82,20 @@ public class HashTable<K, V> {
 
     private int hash(K key) {
         return (key.hashCode() & 0x7FFFFFFF) % M;
+    }
+
+    private void resize(int newM) {
+        TreeMap<K, V>[] newHashTable = new TreeMap[newM];
+        //初始化行的treeMap数组
+        for (int i = 0; i < newM; i++)
+            newHashTable[i] = new TreeMap<>();
+        int oldM = M;
+        this.M = newM;
+        for (int i = 0; i < oldM; i++) {
+            TreeMap<K, V> map = hashtable[i];
+            for (Map.Entry<K, V> entry : map.entrySet()) {
+                newHashTable[hash(entry.getKey())].put(entry.getKey(), entry.getValue());
+            }
+        }
     }
 }
